a5cc6d96f06b70ed573e2ccfbc7aaf260fa7e157
cjm0611 <cjm-0611@naver.com>
2024-08-19 15:25:41 +0900

    refactor: Home을 라우터의 루트 경로로 변경

--- src/App.tsx
+++ src/App.tsx
@@ -21,7 +21,7 @@ function App() {
       <Header />
       <BrowserRouter>
         <Routes>
-          <Route path="/home" element={<Home />} />
+          <Route path="/" element={<Home />} />
         </Routes>
       </BrowserRouter>
     </div>
