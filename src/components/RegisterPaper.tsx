cca136d18cfb3694ac5de042aa2e54cedd5f5a3b
SSUHYUNKIM <yellosuhyun@naver.com>
2024-08-26 13:18:19 +0900

    [FEAT] write loading URL 작업 (#15)

--- src/components/RegisterPaper.tsx
+++ src/components/RegisterPaper.tsx
@@ -17,7 +17,7 @@ enum ModalType {
 
 const RegisterPaper = () => {
   const [url, setUrl] = useState('');
-  const [urlStatus, setUrlStatus] = useState<UrlStatus>(UrlStatus.Invalid);
+  const [urlStatus, setUrlStatus] = useState<UrlStatus>(UrlStatus.Loading);
   const [content, setContent] = useState('');
   const [tags, setTags] = useState('');
   const [tagList, setTagList] = useState<string[]>([]);
@@ -153,7 +153,19 @@ const RegisterPaper = () => {
               {/* URL 상태 메시지 */}
               <div className="flex-shrink-0 flex flex-row justify-center items-center gap-y-0 px-0 py-[3px] overflow-hidden">
                 {urlStatus === UrlStatus.Loading && (
-                  <span className="text-blue-500 text-sm">정보를 가져오는 중...</span>
+                  <div className="flex-shrink-0 flex flex-row justify-end items-end">
+                    <div className="w-[14px] h-[14px] relative">
+                      <img
+                        // To-do: 이미지 URL을 변경하세요.
+                        src="https://image-resource.creatie.ai/135366163293663/135366179022307/f35ed7a9d768ed64c3166075fb683bbe.png"
+                        className="absolute inset-0 w-full h-full object-cover"
+                        alt="Loading icon"
+                      />
+                    </div>
+                    <span className="flex-shrink-0 text-[#B3B3B3] whitespace-nowrap text-[10px] leading-[14px] font-semibold flex items-center">
+                      정보를 가져오는 중입니다
+                    </span>
+                  </div>
                 )}
                 {urlStatus === UrlStatus.Valid && (
                   <div className="flex-shrink-0 flex flex-row justify-end items-end">
@@ -161,11 +173,11 @@ const RegisterPaper = () => {
                       <img
                         src="https://image-resource.creatie.ai/135366163293663/135366179022307/f35ed7a9d768ed64c3166075fb683bbe.png"
                         className="absolute inset-0 w-full h-full object-cover"
-                        alt="Check icon"
+                        alt="Valid icon"
                       />
                     </div>
                     <span className="flex-shrink-0 text-[#AC96D9] whitespace-nowrap text-[10px] leading-[14px] font-semibold flex items-center">
-                      유효한 URL입니다.
+                      유효한 URL입니다
                     </span>
                   </div>
                 )}
@@ -175,11 +187,11 @@ const RegisterPaper = () => {
                       <img
                         src="https://image-resource.creatie.ai/135366163293663/135366179022307/722dc7a1c85a7cd79886c0522a954a38.png"
                         className="absolute inset-0 w-full h-full object-cover"
-                        alt="Check icon"
+                        alt="Invalid icon"
                       />
                     </div>
                     <span className="flex-shrink-0 text-[#B3261E] whitespace-nowrap text-[10px] leading-[14px] font-semibold flex items-center">
-                      URL을 다시 한번 확인해주세요.
+                      URL을 다시 한번 확인해주세요
                     </span>
                   </div>
                 )}
