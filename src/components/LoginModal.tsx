5f1e7976a96e440d41fd82d10bf8a741619252c0
cjm0611 <cjm-0611@naver.com>
2024-08-30 12:21:50 +0900

    refactor: 로그인 확인 로직 수정
    로그인이 되어 있지 않을 때만 checkLoginStatus 호출
    user dto를 이용해 store의 nickname, profileImage 관리

--- src/components/LoginModal.tsx
+++ src/components/LoginModal.tsx
@@ -3,6 +3,7 @@ import KakaoLoginIcon from '../assets/png/KakaoLoginIcon.png';
 import { ReactComponent as LogoIcon } from '../assets/svg/LogoIcon.svg';
 import useAuthStore from '../stores/useAuthStore';
 import axios from 'axios';
+import { UserDTO } from '../types/dto/user/UserDTO';
 
 const LoginModal = () => {
   const isLoggedIn = useAuthStore(state => state.isLoggedIn);
@@ -15,16 +16,16 @@ const LoginModal = () => {
           withCredentials: true,
         });
 
-        login(response.data.nickname, response.data.profileImage);
+        const userInfo: UserDTO = response.data;
+        login(userInfo.nickname, userInfo.profileImage);
       } catch (error) {
-        console.error('로그인 상태 확인 실패:', error);
+        console.error('User API 요청 실패:', error);
       }
     };
 
-    checkLoginStatus();
-
     if (!isLoggedIn) {
-      document.body.style.overflow = 'hidden'; // 모달이 열릴 때 스크롤 비활성화
+      checkLoginStatus();
+      document.body.style.overflow = 'hidden'; // 스크롤 비활성화
     } else {
       document.body.style.overflow = '';
     }
@@ -42,25 +43,21 @@ const LoginModal = () => {
     }
   };
 
-  return (
-    <div>
-      {!isLoggedIn && (
-        <div className="fixed inset-0 z-10">
-          <div className="absolute inset-0 bg-system-white opacity-50"></div>
-          <div className="relative flex items-center justify-center h-full">
-            <div className="max-w-sm bg-system-white border border-line-1 px-[30px] py-[36px] shadow-[0_4px_10px_0_rgba(0,0,0,0.1)] rounded-[8px] flex flex-col items-center justify-center">
-              <LogoIcon width={145} height={35} fill="#1E1E1E" />
-              <p className="mt-[24px] text-center">
-                간편하게 로그인하고
-                <br /> 다른 사람들과 이야기를 나눠보세요
-              </p>
-              <button className="mt-[40px]" onClick={handleLogin}>
-                <img src={KakaoLoginIcon} alt="Kakao Login" />
-              </button>
-            </div>
-          </div>
+  return isLoggedIn ? null : (
+    <div className="fixed inset-0 z-10">
+      <div className="absolute inset-0 bg-system-white opacity-50"></div>
+      <div className="relative flex items-center justify-center h-full">
+        <div className="max-w-sm bg-system-white border border-line-1 px-[30px] py-[36px] shadow-[0_4px_10px_0_rgba(0,0,0,0.1)] rounded-[8px] flex flex-col items-center justify-center">
+          <LogoIcon width={145} height={35} fill="#1E1E1E" />
+          <p className="mt-[24px] text-center">
+            간편하게 로그인하고
+            <br /> 다른 사람들과 이야기를 나눠보세요
+          </p>
+          <button className="mt-[40px]" onClick={handleLogin}>
+            <img src={KakaoLoginIcon} alt="Kakao Login" />
+          </button>
         </div>
-      )}
+      </div>
     </div>
   );
 };
