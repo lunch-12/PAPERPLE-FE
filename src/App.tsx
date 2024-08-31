b376548a5a042a2ad0e8ed4104045bf122e68cb6
SSUHYUNKIM <yellosuhyun@naver.com>
2024-08-31 16:53:08 +0900

    [REFACTOR] 충돌 해결(#20)

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header';
-import Home from './pages/Home';
+import HomePage from './pages/HomePage';
 import SearchPage from './pages/SearchPage';
 import RegisterPaper from './pages/RegisterPaper';
 import { useEffect } from 'react';
 import setScreenHeight from './utils/setScreenHeight';
 import 'pretendard/dist/web/static/pretendard.css';
+import LoginModal from './components/LoginModal';
 
 function App() {
   useEffect(() => {
@@ -22,8 +23,9 @@ function App() {
     <div className="w-full max-w-sm mx-auto h-real-screen">
       <BrowserRouter>
         <Header />
+        <LoginModal />
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<HomePage />} />
           <Route path="/search" element={<SearchPage />} />
           <Route path="/register-paper" element={<RegisterPaper />} />
         </Routes>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,7 @@
 import { ReactComponent as SettingIcon } from '../assets/svg/SettingIcon.svg';
 import { ReactComponent as NotificationIcon } from '../assets/svg/NotificationIcon.svg';
 import { ReactComponent as SearchIcon } from '../assets/svg/SearchIcon.svg';
+import { ReactComponent as LogoIcon } from '../assets/svg/LogoIcon.svg';
 import { useNavigate } from 'react-router-dom';
 
 const Header = () => {
@@ -12,7 +13,7 @@ const Header = () => {
 
   return (
     <header className="w-full h-[64px] px-[24px] bg-system-white flex items-center justify-between">
-      <p className="text-[22px] text-system-black font-extrabold">Paperple</p>
+      <LogoIcon width={88} height={21} fill="#1E1E1E" />
       <div className="flex items-center justify-center space-x-1">
         <SearchIcon width={28} className="cursor-pointer" onClick={handleSearchIconClick} />
         <NotificationIcon width={28} />
