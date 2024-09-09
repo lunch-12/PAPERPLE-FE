bab8030beb9dd928a072ff6c377c77c1898865c2
cjm0611 <cjm-0611@naver.com>
2024-09-10 08:59:10 +0900

    feat: 로고 아이콘 클릭 시 메인으로 이동, 설정 아이콘 제거

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,3 @@
-import { ReactComponent as SettingIcon } from '../assets/svg/SettingIcon.svg';
 import { ReactComponent as NotificationIcon } from '../assets/svg/NotificationIcon.svg';
 import { ReactComponent as SearchIcon } from '../assets/svg/SearchIcon.svg';
 import { ReactComponent as LogoIcon } from '../assets/svg/LogoIcon.svg';
@@ -7,13 +6,19 @@ import { useNavigate } from 'react-router-dom';
 const Header = () => {
   const navigate = useNavigate();
 
+  const handleLogoClick = () => {
+    navigate('/');
+  };
+
   const handleSearchIconClick = () => {
     navigate('/search');
   };
 
   return (
     <header className="w-full h-[64px] px-[24px] bg-system-white flex items-center justify-between">
-      <LogoIcon width={88} height={21} fill="#1E1E1E" />
+      <button onClick={handleLogoClick}>
+        <LogoIcon width={88} height={21} fill="#1E1E1E" />
+      </button>
       <div className="flex items-center justify-center space-x-1">
         <SearchIcon
           width={28}
@@ -21,7 +26,6 @@ const Header = () => {
           onClick={handleSearchIconClick}
         />
         <NotificationIcon width={28} />
-        <SettingIcon width={28} />
       </div>
     </header>
   );
