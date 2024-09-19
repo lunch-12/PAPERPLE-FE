1ee5b872a0730c38099187ff9fba1781ed40c145
SSUHYUNKIM <yellosuhyun@naver.com>
2024-09-19 15:19:16 +0900

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,2 @@
-export const API_BASE_URL = 'https://paperple.p-e.kr';
+export const API_BASE_URL = 'paperple.p-e.kr';
 export const STOCK_API_SECRET_KEY = process.env.REACT_APP_STOCK_API_SECRET_KEY;
